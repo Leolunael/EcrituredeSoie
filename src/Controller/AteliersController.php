26017e74e907d795fe611f706257095408dedbcf
leolunael <leolunael@gmail.com>
2026-04-17 09:06:44 +0200

    Modifs

--- src/Controller/AteliersController.php
+++ src/Controller/AteliersController.php
@@ -24,7 +24,7 @@ class AteliersController extends AbstractController
 {
     // ==================== PAGE D'ACCUEIL (TOUT) ====================
 
-    #[Route('/inscriptions', name: 'app_ateliers')]
+    #[Route('/inscription', name: 'app_ateliers')]
     public function list(
         AtelierRepository $atelierRepo,
         VisioRepository $visioRepo,
@@ -60,7 +60,7 @@ class AteliersController extends AbstractController
 
     // ==================== ATELIERS ====================
 
-    #[Route('/inscriptions/{id}', name: 'app_atelier_show', requirements: ['id' => '\d+'])]
+    #[Route('/inscription/{id}', name: 'app_atelier_show', requirements: ['id' => '\d+'])]
     public function show(
         int $id,
         Request $request,
